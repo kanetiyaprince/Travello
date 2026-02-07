e46fa07bbde153d8c2cf5e6714c48ac3ff7ada26
Prince <princepatel25125@gmail.com>
2026-02-07 19:24:55 +0530

    updated

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,7 @@
-pip install -r requirements.txt
+# build_files.sh
+
+# Install dependencies with the break-system-packages flag
+python3.9 -m pip install -r requirements.txt --break-system-packages
+
+# Collect static files
 python3.9 manage.py collectstatic --noinput --clear
